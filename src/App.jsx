cabe49965c9da6e85ef8edcd32632d2e6ab8d366
Aftab Alam <aftabf8982@gmail.com>
2024-05-08 18:15:59 +0530

    added todo list table and remove debouce feture from add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Table from "./components/Table";
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState(["need to complete task", "get vegetables"]);
+  const [text, setText] = useState("");
   const [timeoutId, setTimeOutId] = useState(null);
+  const inputRef = useRef();
+
   const handleClick = () => {
-    console.log("clicked");
+    if (text) {
+      setData([...data, text]);
+      inputRef.current.value = "";
+    }
   };
   const handleChange = (value) => {
     console.log(value);
+    setText(value);
   };
   const debounce = (callback, delay, value) => {
     clearTimeout(timeoutId);
@@ -17,20 +25,25 @@ function App() {
     }, delay);
     setTimeOutId(id);
   };
-
   return (
-    <div className='flex justify-center items-center pt-20 w-full'>
-      <input
-        type='text'
-        className='py-1 px-2 w-3/6 text-2xl outline-none rounded-l-md'
-        onChange={(e) => debounce(handleChange, 500, e.target.value)}
-      />
-      <button
-        onClick={() => debounce(handleClick, 200)}
-        className=' font-bold bg-slate-400 text-white py-2 px-3 rounded-r-lg hover:bg-slate-500 active:bg-slate-600'
-      >
-        Click
-      </button>
+    <div className='flex flex-col justify-center items-center pt-20 w-screen'>
+      <div className='flex justify-center w-1/2'>
+        <input
+          ref={inputRef}
+          type='text'
+          className='py-1 px-2  text-2xl outline-none rounded-l-md w-full'
+          onChange={(e) => debounce(handleChange, 500, e.target.value)}
+        />
+        <button
+          type='button'
+          onClick={() => debounce(handleClick, 500)}
+          className=' font-bold bg-slate-500 text-white py-2 px-3 rounded-r-lg hover:bg-slate-400 active:bg-slate-600 active:translate-y-0.5'
+        >
+          ADD
+        </button>
+      </div>
+
+      <Table data={data} />
     </div>
   );
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Table = ({ data }) => {
+  //   console.log(data);
+  //   const { tableData } = data;
+  //   const tableData = ["need to complete task", "get vegetables"];
+  return (
+    <div className='mt-6 flex flex-col w-1/2 '>
+      <table className='min-w-full divide-y divide-gray-200 '>
+        <thead className='bg-gray-50'>
+          <tr>
+            <th
+              scope='col'
+              className='px-12 py-3.5 text-left text-xl font-bold text-gray-700'
+            >
+              Todo List
+            </th>
+            <th scope='col' className='relative px-4 py-3.5'>
+              <span className='sr-only'>Edit</span>
+            </th>
+            <th scope='col' className='relative px-4 py-3.5'>
+              <span className='sr-only'>Delete</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody className='divide-y divide-gray-200 bg-white'>
+          {data.map((item) => (
+            <tr key={item}>
+              <td className='whitespace-nowrap px-12 py-4'>
+                <div className='text-xl text-gray-900 '>{item}</div>
+              </td>
+
+              <td className='whitespace-nowrap px-1 py-2 text-sm text-gray-700'>
+                <button className='text-gray-700 text-lg px-2 py-1 rounded-md active:bg-slate-200 active:translate-y-0.5'>
+                  Edit
+                </button>
+              </td>
+              <td className='whitespace-nowrap px-1 py-2 text-right text-sm font-medium'>
+                <button className='text-gray-700 text-lg px-2 py-1 rounded-md active:bg-slate-200 active:translate-y-0.5'>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
